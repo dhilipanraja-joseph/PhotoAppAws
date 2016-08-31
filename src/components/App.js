24cb0a505d2c4036134d2e5fd364d2abec8874fb
dhilipanraja-joseph <dlipnraj@gmail.com>
2016-08-30 19:27:20 -0700

    add routes

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ export default class App extends Component{
   render(){
     return (
       <div className="container">
-        <h1>PhotoAPP</h1>
+        <Link to="/"><h1>PhotoAPP</h1></Link>
         <Link to="/upload">Upload Images</Link><span> </span>
         <Link to="/viewImages">Uploaded Images</Link>
         {this.props.children}
